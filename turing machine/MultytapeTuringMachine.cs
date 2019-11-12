4c86a17b1a0b4397d1ea7ad8f2962dbf62a3aeeb
DaniilAfendulov <daniilafendulov@bk.ru>
2019-11-13 01:01:29 +0300

    TuringMachine add as separate class

---
+++ turing machine/MultytapeTuringMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static turing_machine.Program;
+
+namespace turing_machine
+{
+    class MultytapeTuringMachine
+    {
+        List<TuringMachine> turingMachines;
+
+    }
+}
---
+++ turing machine/TuringMachine.cs
@@ -0,0 +1,113 @@
+﻿//Автор: Афендулов Даниил 
+//Реализация Машины Тьюринга
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace turing_machine
+{
+    partial class Program
+    {
+        public class TuringMachine
+        {
+            List<char> _word;
+            int _index;
+            int _q;
+            Dictionary<int, Dictionary<char, Command>> _tacts;
+
+            public TuringMachine(string word)
+            {
+                _word = new List<char>();
+                _word.AddRange((" " + word + " ").ToCharArray());
+                _index = 1;
+                _q = 1;
+                _tacts = new Dictionary<int, Dictionary<char, Command>>();
+            }
+
+            public char[] Execute()
+            {
+                ExecuteCommand(_tacts[_q][_word[_index]]);
+                return _word.ToArray();
+            }
+
+            void ExecuteCommand(Command command)
+            {
+                _word[_index] = command.symbol;
+                switch (command.direct)
+                {
+                    case Direct.R:
+                        _index++;
+                        if (_index == _word.Count) _word.Add(' ');
+                        break;
+                    case Direct.L:
+                        _index--;
+                        if (_index == 0) _word.Insert(0, ' ');
+                        break;
+                    case Direct.N:
+                        break;
+                    default:
+                        throw new Exception("invalid direct");
+                }
+                _q = command.q;
+                if (_q == 0) return;
+                ExecuteCommand(_tacts[_q][_word[_index]]);
+            }
+
+            public bool AddCommand(char s, int q, Command command)
+            {
+                if (!_tacts.ContainsKey(q)) _tacts.Add(q, new Dictionary<char, Command>());
+                if (!_tacts[q].ContainsKey(s))
+                {
+                    _tacts[q].Add(s, command);
+                    return true;
+                }
+                return false;
+            }
+
+            static public void StartTuringProgramFromFile(string filePath)
+            {
+                TuringMachine turingMachine = null;
+                using (StreamReader file = new StreamReader(filePath))
+                {
+                    while (!file.EndOfStream)
+                    {
+                        if ((char)file.Peek() == '#') file.ReadLine();
+                        else
+                        {
+                            string line = file.ReadLine();
+                            if (!string.IsNullOrWhiteSpace(line))
+                            {
+                                if (line.Contains("word"))
+                                {
+                                    int index = line.IndexOf('=');
+                                    turingMachine = new TuringMachine(line.Substring(++index));
+                                }
+                                else if (turingMachine != null)
+                                {
+                                    string[] parts = line.Split(new char[]{ ',', ':' } );
+                                    int current_state = Convert.ToInt32(parts[1]);
+                                    char current_symbol = parts[0].ToCharArray()[0];
+
+                                    char new_symbol = parts[2].ToCharArray()[0];
+                                    int new_state = Convert.ToInt32(parts[3]);
+                                    Direct direct = Direct.N;
+                                    if (parts[4] == "R") direct = Direct.R;
+                                    else if (parts[4] == "L") direct = Direct.L;
+
+                                    turingMachine.AddCommand(current_symbol, current_state, new Command(new_symbol, new_state, direct));
+                                }
+                            }
+                        }
+
+                    }
+                }
+                Console.WriteLine(turingMachine.Execute());
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
